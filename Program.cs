66489bc60954c04d5d0a06608cbfb154a296f12f
AmitShech <amitshechnik@gmail.com>
2025-02-16 14:26:31 +0200

    move the main function to SudokuManager

--- Program.cs
+++ Program.cs
@@ -6,54 +6,11 @@ using static Sudoku.Ui;
 
 namespace Sudoku
 {
-
-    /// <summary>
-    /// The entry point of the application. This class orchestrates the main program flow:
-    /// reading input, validating, solving the board, and displaying results.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// Main method: repeatedly gets board input from the user, validates it,
-        /// creates a board, solves it, and displays the result.
-        /// </summary>
         public static void Main(string[] args)
         {
-            InputValidator.CtrlCHandler();
-
-            Console.WriteLine("WELCOME TO SUDOKU SOLVER!");
-            Console.WriteLine("Please enter a Sudoku board:");
-            Console.WriteLine(" - You can enter a string representing the board");
-            Console.WriteLine(" - Or provide a path to a text file (txt) containing the board");
-
-            while (true)
-            {
-
-                string input = Ui.GetBoardInput();
-                if (input == null)
-                    break;
-
-                if (!InputValidator.IsValidInput(input)) { continue; }
-
-                Board board = new Board(input);
-
-                if (!BoardValidator.IsBoardValid(board)) { continue; }
-                    
-                Ui.BoardToGrid(board);
-
-
-                Stopwatch stopwatch = Stopwatch.StartNew();
-
-                SudokuSolver solver = new SudokuSolver(board);
-                bool solved = solver.Solve();
-
-                stopwatch.Stop();
-                long elapsedTime = stopwatch.ElapsedMilliseconds;
-
-                Ui.DisplayResult(board, solved, elapsedTime);
-            }
-
-            Console.WriteLine("\nProgram ended. Thank you and goodbye!");
+            SudokuManager.Run();
         }
 
         
